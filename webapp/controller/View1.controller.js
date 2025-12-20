d01c07a6b4360ce30bb435b687d89f0d2825036c
Gunjan Sharma <gunjan.designer25@gmail.com>
2025-12-20 08:46:18 +0000

    two way binding

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,16 +7,30 @@ sap.ui.define([
     return Controller.extend("com.emp.employeeform.controller.View1", {
         onInit() {
            this.oView = this.getView();
-
+        // calling our own reuse class to create model object
            var oModel =  Models.createJSONModel();
 
            this.oView.setModel(oModel);
         },
         onReload : function (){
-        this.oView.byId("idEmpId").setValue("");
-        this.oView.byId("idEmpName").setValue("");
-        this.oView.byId("idEmpSal").setValue("");
-        this.oView.byId("idEmpCurr").setValue("");
+        // step 1 :- get the model object
+        // var oModel = sap.ui.getCore().getModel();
+         var oModel = this.oView.getModel();
+        // step 2 :- change data in the model
+        var oData = oModel.getProperty("/empSTR");
+        console.log(oData);
+        oModel.setProperty("/empSTR/empName","Spiderman");
+        // this.oView.byId("idEmpId").setValue("");
+        // this.oView.byId("idEmpName").setValue("");
+        // this.oView.byId("idEmpSal").setValue("");
+        // this.oView.byId("idEmpCurr").setValue("");
+        },
+        onShowme : function(){
+        // var oModel = sap.ui.getCore().getModel();
+         var oModel = this.oView.getModel();
+        // step 2 :- change data in the model
+        var oData = oModel.getProperty("/");
+        console.log(oData);
         }
     });
 });
